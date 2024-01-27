95aa075f564dbeb680a50de5914733d0b63fefaf
AdnanSameer24 <adnansameerazk24@gmail.com>
2024-01-27 00:03:30 -0800

    ad a controller and router for home

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const app = express();
 const port = 8000;
 
 
-
-const app = express();
+//use express router
+app.use('/', require('./routes'));
 
 
 app.listen(port, function(err){
