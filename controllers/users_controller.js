deb3b7eb2f973aef6c0121c2f6818e7e0d3337f2
AdnanSameer24 <adnansameerazk24@gmail.com>
2024-02-04 00:13:02 -0800

    adding flash msgs for signing in and logged out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -144,11 +144,14 @@ module.exports.create = async function (req, res) {
 
 module.exports.createSession = function(req, res){
     //
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
+  req.flash('success', 'You have logged out :)');
   req.logout(function(err){
+    
     return res.redirect('/');
   });
 
